5969b1cae0a836c2a682da982ccf2d37b12bcf37
wangruohang <wangruoxing@huoyunren.com>
2017-05-12 16:36:18 +0800

    change gitignore

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -4,7 +4,7 @@
 <div class="b-inner-page-header f-inner-page-header b-bg-header-inner-page">
     <div class="b-inner-page-header__content">
         <div class="container">
-            <h1 class="f-primary-l c-default">Blog Wrh's life record</h1>
+            <h1 class="f-primary-l c-default">Recording</h1>
         </div>
     </div>
 </div>
