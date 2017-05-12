4eae06c823bd90a8e0eb80fd9583438311065781
wangruohang <wangruoxing@huoyunren.com>
2017-05-12 14:31:31 +0800

    change mysql

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => '119.23.19.236',
+            'port' => '3306',
+            'database' => 'blog',
+            'username' => 'wrh',
+            'password' => '123456',
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
